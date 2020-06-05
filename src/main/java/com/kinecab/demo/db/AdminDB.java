8c6c4d292018fd0920839d840798ffe9c055e2ac
unknown <stephaneferreira75@gmail.com>
2020-06-06 01:23:02 +0200

    bug fixes

--- src/main/java/com/kinecab/demo/db/AdminDB.java
+++ src/main/java/com/kinecab/demo/db/AdminDB.java
@@ -16,6 +16,8 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.NativeQuery;
 
+import javax.persistence.NoResultException;
+
 
 public class AdminDB {
 
@@ -24,7 +26,7 @@ public class AdminDB {
     //~ ----------------------------------------------------------------------------------------------------------------
 
     public static void saveAdmin(Admin admin) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction trx = session.beginTransaction();
             session.saveOrUpdate(admin);
             trx.commit();
@@ -32,7 +34,7 @@ public class AdminDB {
     }
 
     public static List<Admin> checkPasswordByTokenAdmin(String token, String password) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             NativeQuery sqlQuery = session.createSQLQuery("SELECT * from TOKEN where  TOKEN.token = '" + token + "';");
             List<Token> list = sqlQuery.addEntity(Token.class).list();
             if (!list.isEmpty()) {
@@ -44,7 +46,7 @@ public class AdminDB {
     }
 
     public static void removeAdmin(Admin admin) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction trx = session.beginTransaction();
             session.remove(admin);
             trx.commit();
@@ -53,7 +55,7 @@ public class AdminDB {
 
     public static String getTokenAdmin(Admin admin) {
         Token token = new Token(admin.getId(), Hashing.sha256().hashString(admin.getPassword() + admin.getId(), StandardCharsets.UTF_8).toString(), "1");
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction trx = session.beginTransaction();
             session.saveOrUpdate(token);
             trx.commit();
@@ -62,7 +64,7 @@ public class AdminDB {
     }
 
     public static List<Colab> getColabByToken(String token) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             NativeQuery sqlQuery = session.createSQLQuery("SELECT * from TOKEN where  TOKEN.token = '" + token + "';");
             List<Token> list = sqlQuery.addEntity(Token.class).list();
             if (!list.isEmpty()) {
@@ -74,7 +76,7 @@ public class AdminDB {
     }
 
     public static List<Admin> getAdminByToken(String token) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             NativeQuery sqlQuery = session.createSQLQuery("SELECT * from TOKEN where  TOKEN.token = '" + token + "';");
             List<Token> list = sqlQuery.addEntity(Token.class).list();
             if (!list.isEmpty()) {
@@ -86,31 +88,34 @@ public class AdminDB {
     }
 
     public static List<Colab> getColabs() {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             NativeQuery sqlQuery = session.createSQLQuery("SELECT * from COLAB ;");
-                return sqlQuery.addEntity(Colab.class).list();
-            }
+            return sqlQuery.addEntity(Colab.class).list();
         }
+    }
 
     public static List<Admin> checkPasswordByEmailAdmin(String email, String password) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             NativeQuery sqlQuery = session.createSQLQuery("SELECT * from ADMIN where ADMIN.password = '" + password + "' and  ADMIN.email = '" + email + "';");
             return sqlQuery.addEntity(Admin.class).list();
         }
     }
 
     public static List<Person> getPersonByIdCab(int idCab) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             List<Person> people = new ArrayList<>();
             NativeQuery sqlQuery = session.createSQLQuery("SELECT * FROM CAB_PERSON WHERE  CAB_PERSON.idCab = '" + idCab + "';");
             List<CabPerson> cabPerson = (List<CabPerson>) sqlQuery.addEntity(CabPerson.class).list();
-
-            cabPerson.stream().forEach(person -> {
-                final NativeQuery sqlQuery1 = session.createSQLQuery("SELECT * FROM PERSON WHERE  PERSON.id = '" + person.getIdPerson() + "';");
-                final Person singleResult = (Person) sqlQuery1.addEntity(Person.class).getSingleResult();
-                singleResult.setPassword("");
-                people.add(singleResult);
-            });
+            try {
+                cabPerson.stream().forEach(person -> {
+                    final NativeQuery sqlQuery1 = session.createSQLQuery("SELECT * FROM PERSON WHERE  PERSON.id = '" + person.getIdPerson() + "';");
+                    final Person singleResult = (Person) sqlQuery1.addEntity(Person.class).getSingleResult();
+                    singleResult.setPassword("");
+                    people.add(singleResult);
+                });
+            } catch (NoResultException nre) {
+                //Ignore this because as per your logic this is ok!
+            }
             return people;
         }
     }
